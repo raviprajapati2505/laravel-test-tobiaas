c9a13ca5242d3378af1635b9bcfc8ef30647a8ff
raviprajapati2505 <106593223+raviprajapati2505@users.noreply.github.com>
2022-06-24 18:59:04 +0530

    added migration for cinema
    added migration for cinema

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -6,38 +6,91 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCinemaSchema extends Migration
 {
-    /**
-    # Create a migration that creates all tables for the following user stories
-
-    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-    To not introduce additional complexity, please consider only one cinema.
-
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
-
-    ## User Stories
-
-     **Movie exploration**
-     * As a user I want to see which films can be watched and at what times
-     * As a user I want to only see the shows which are not booked out
-
-     **Show administration**
-     * As a cinema owner I want to run different films at different times
-     * As a cinema owner I want to run multiple films at the same time in different locations
-
-     **Pricing**
-     * As a cinema owner I want to get paid differently per show
-     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
-
-     **Seating**
-     * As a user I want to book a seat
-     * As a user I want to book a vip seat/couple seat/super vip/whatever
-     * As a user I want to see which seats are still available
-     * As a user I want to know where I'm sitting on my ticket
-     * As a cinema owner I dont want to configure the seating for every show
-     */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->datetime('duration');
+            $table->string('language');
+            $table->datetime('released_date');
+            $table->string('country');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_seats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('seat_number');
+            $table->integer('type');
+            $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->datetime('start_time');
+            $table->datetime('end_time');
+            $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->constrained()->onDelete('cascade');
+            $table->foreign('movie_id')->references('id')->on('movies')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number_of_seats');
+            $table->datetime('timestamp');
+            $table->integer('status');
+            $table->foreign('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
+            $table->foreign('show_id')->references('id')->on('shows')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinemas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_cinema_halls');
+            $table->foreign('city_id')->references('id')->on('cities')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_halls', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_seats');
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('show_seats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status');
+            $table->integer('price');
+            $table->foreign('cinema_seat_id')->references('id')->on('cinema_seats')->constrained()->onDelete('cascade');
+            $table->foreign('show_id')->references('id')->on('shows')->constrained()->onDelete('cascade');
+            $table->foreign('booking_id')->references('id')->on('bookings')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('amount');
+            $table->datetime('timestamp');
+            $table->integer('discount_coupon_id');
+            $table->integer('remote_transaction_id');
+            $table->integer('payment_method');
+            $table->foreign('booking_id')->references('id')->on('bookings')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -47,5 +100,14 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('shows');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('cinemas');
+        Schema::dropIfExists('cinema_halls');
+        Schema::dropIfExists('cinema_seats');
+        Schema::dropIfExists('cities');
+        Schema::dropIfExists('show_seats');
+        Schema::dropIfExists('payments');
     }
 }
