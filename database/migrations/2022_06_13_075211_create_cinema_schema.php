8cd830185bad4c0ee8cd7abe8c5dd84d47713568
raviprajapati2505 <106593223+raviprajapati2505@users.noreply.github.com>
2022-06-24 19:21:51 +0530

    changes in cinema migration
    changes in cinema migration

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -19,10 +19,38 @@ class CreateCinemaSchema extends Migration
             $table->timestamps();
         });
 
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->timestamps();
+        });
+
+        Schema::create('cinemas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_cinema_halls');
+            $table->bigInteger('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        
+        Schema::create('cinema_halls', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_seats');
+            $table->bigInteger('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
         Schema::create('cinema_seats', function (Blueprint $table) {
             $table->id();
             $table->integer('seat_number');
             $table->integer('type');
+            $table->bigInteger('cinema_hall_id')->unsigned();
             $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
@@ -32,49 +60,35 @@ class CreateCinemaSchema extends Migration
             $table->date('date');
             $table->datetime('start_time');
             $table->datetime('end_time');
+            $table->bigInteger('cinema_hall_id')->unsigned();
+            $table->bigInteger('movie_id')->unsigned();
             $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->constrained()->onDelete('cascade');
             $table->foreign('movie_id')->references('id')->on('movies')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
 
-        Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('state');
-            $table->string('zipcode');
-            $table->timestamps();
-        });
+
 
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->integer('number_of_seats');
             $table->datetime('timestamp');
             $table->integer('status');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('show_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
             $table->foreign('show_id')->references('id')->on('shows')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
 
-        Schema::create('cinemas', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->integer('total_cinema_halls');
-            $table->foreign('city_id')->references('id')->on('cities')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
-
-        Schema::create('cinema_halls', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->integer('total_seats');
-            $table->foreign('cinema_id')->references('id')->on('cinemas')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
 
         Schema::create('show_seats', function (Blueprint $table) {
             $table->id();
             $table->integer('status');
             $table->integer('price');
+            $table->bigInteger('cinema_seat_id')->unsigned();
+            $table->bigInteger('show_id')->unsigned();
+            $table->bigInteger('booking_id')->unsigned();
             $table->foreign('cinema_seat_id')->references('id')->on('cinema_seats')->constrained()->onDelete('cascade');
             $table->foreign('show_id')->references('id')->on('shows')->constrained()->onDelete('cascade');
             $table->foreign('booking_id')->references('id')->on('bookings')->constrained()->onDelete('cascade');
@@ -88,6 +102,7 @@ class CreateCinemaSchema extends Migration
             $table->integer('discount_coupon_id');
             $table->integer('remote_transaction_id');
             $table->integer('payment_method');
+            $table->bigInteger('booking_id')->unsigned();
             $table->foreign('booking_id')->references('id')->on('bookings')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
